d89589ac566294593260b9d25798f93bf604f204
Poonam Digole <poonamdigole123@gmail.com>
2024-04-25 00:35:08 +0530

    created routers

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import './index.css';
 import SignUp from './views/SignUp/SignUp';
-// import Contact from './views/Contact/Contact';
+// import Login from './views/Login/Login'
+// import Plans from './views/Plans/Plans'
+import Contact from './views/Contact/Contact';
+// import Home from './views/Home/Home'
+// import About from './views/About/About'
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <SignUp/>
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  // {
+  //   path: '/',
+  //   element: <Home/>
+  // },
+  // {
+  //   path: '/about',
+  //   element: <About />
+  // },
+  // {
+  //   path: '/login',
+  //   element: <Login />
+  // },
+  // {
+  //   path: '/plans',
+  //   element: <Plans/>
+  // },
+
+  {
+    path: '/signup',
+    element: <SignUp />
+  },
+ 
+  {
+    path:"/contact",
+    element:<Contact/>
+  }
+])
+
+root.render(<RouterProvider router={router} />);
+
+
+
