5259220cc7ba6b54b9bc9c8c31dcc0db4b861b01
Poonam Digole <poonamdigole123@gmail.com>
2024-04-24 22:54:21 +0530

    added contact page

---
+++ client/src/views/Contact/Contact.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import './Contact.css'
+function Contact() {
+  return (
+   <>
+   
+   <div className="z">
+        <h3 className="l ">CONTACT WITH US</h3>
+       <p className="x text-danger fs-3">We are just step Away...</p>
+
+    </div>
+    {/* <!--card--> */}
+   
+   <br/>
+    <div className="p">
+    <div className="card border-danger bg-black " >
+        <div className="card-header bg-danger border-danger">CONTACT</div>
+        <br/>
+        <div className="card-body">
+            <h5 className="card-title">ADDRESS</h5>
+            <p className="card-text">Asset No.6,near International Airport,Moscat,Australia </p>
+          </div>
+          <hr class="m text-danger-emphasis "/>
+          <div class="card-body">
+            <h5 class="card-title">PHONE</h5>
+            <p class="card-text">011 46050102</p>
+          </div><hr class="m text-danger-emphasis"/>
+          <div className="card-body ">
+            <h5 className="card-title">EMAIL</h5>
+            <p className="card-text">reservation@royalhotel.com</p>
+          </div>
+          </div>
+          {/* <!--map--> */}
+        <div class="img-map my-4">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d120637.94989627846!2d74.66027112783725!3d19.11046628008788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bdcb05d46788921%3A0x6677e92c1a5181b6!2sAhmednagar%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1713975101344!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <br/>
+    </div>
+    <br/>
+    <br/>
+    {/* <!--form--> */}
+
+    <div className="for-m py-4" >
+    <div className="form bg-none">
+      <h1 class="head text-danger text-center my-4">Get in touch..!</h1>
+      <form class="row g-3">
+        <div class="col-md-6">
+          <label for="validationServer01" class="form-label text-white">First name</label>
+          <input type="text" className="form-control is-valid py-2" id="validationServer01" value="" required placeholder='John'/>
+          <div class="valid-feedback">
+            
+          </div>
+        </div>
+        <div class="col-md-6">
+          <label for="validationServer02" class="form-label text-white">Last name</label>
+          <input type="text" class="form-control py-2 is-valid" id="validationServer02" value="" required placeholder=''/>
+          <div class="valid-feedback">
+         
+          </div>
+        </div>
+        {/* <div class="col-md-4">
+          <label for="validationServerUsername" class="form-label text-white">Username</label>
+          <div class="input-group has-validation">
+            <span class="input-group-text" id="inputGroupPrepend3">@</span>
+            <input type="text" class="form-control is-invalid" id="validationServerUsername" aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback" required />
+            <div id="validationServerUsernameFeedback" class="invalid-feedback">
+              Please choose a username.
+            </div>
+          </div>
+        </div> */}
+
+<label htmlFor="floatingInput" class="form-label text-white ">Email address</label>
+
+             
+            <input type="email" class="form-control py-2" id="floatingInput" placeholder="name@example.com"/>
+            
+          
+         
+          <label for="floatingPassword" class="form-label text-white ">Password</label>
+       
+            <input type="password" class="form-control py-2" id="floatingPassword" placeholder="Password"/>
+          
+        
+        
+     
+            <label for="exampleFormControlTextarea1" class="form-label text-white">Query</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+       
+                  <div class="col-12"/>
+    
+          <div className="col-12">
+            <button className="btn btn-danger" type="submit">CONTACT US</button>
+          </div>
+      </form>
+    </div>
+    </div>
+    
+    
+
+
+   </>
+  )
+}
+
+export default Contact
---
+++ client/src/views/SignUp/SignUp.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import './SignUp.css'
+// import {Link} from 'react'
+// import logo from './../../bigbull-logo.png';
+
+function SignUp() {
+  return (
+    <div>
+
+<div class="main">
+            <div class="box ">
+                <form action="">
+
+                    <div class="border-b border-gray-900/10 pb-12 .form">
+
+                        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                           {/* <img src={logo}/> */}
+                            <h2>SignUp</h2>
+                        </div>
+
+                        <div class="mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                            <div class="sm:col-span-3">
+                                <label for="first-name" class="block text-sm font-medium leading-6 text-white">First
+                                    name</label>
+                                <div class="mt-2">
+                                    <input type="text" name="first-name" id="first-name" autocomplete="given-name"
+                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                                </div>
+                            </div>
+
+                            <div class="sm:col-span-3">
+                                <label for="last-name" class="block text-sm font-medium leading-6 text-white">Last
+                                    name</label>
+                                <div class="mt-2">
+                                    <input type="text" name="last-name" id="last-name" autocomplete="family-name"
+                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                                </div>
+                            </div>
+
+                            <div class="sm:col-span-4">
+                                <label for="email" class="block text-sm font-medium leading-6 text-white">Email
+                                    address</label>
+                                <div class="mt-2">
+                                    <input id="email" name="email" type="email" autocomplete="email"
+                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                                </div>
+                            </div>
+
+                            <div class="sm:col-span-3">
+                                <label for="country"
+                                    class="block text-sm font-medium leading-6 text-white">Country</label>
+                                <div class="mt-2">
+                                    <select id="country" name="country" autocomplete="country-name"
+                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                                        <option>United States</option>
+                                        <option>India</option>
+                                        <option>Canada</option>
+                                        <option>Mexico</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-span-full">
+                                <label for="street-address"
+                                    class="block text-sm font-medium leading-6 text-white">Street
+                                    address</label>
+                                <div class="mt-2">
+                                    <input type="text" name="street-address" id="street-address"
+                                        autocomplete="street-address"
+                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                                </div>
+                            </div>
+
+                            <div class="sm:col-span-2 sm:col-start-1">
+                                <label for="city" class="block text-sm font-medium leading-6 text-white">City</label>
+                                <div class="mt-2">
+                                    <input type="text" name="city" id="city" autocomplete="address-level2"
+                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                                </div>
+                            </div>
+
+                            <div class="sm:col-span-2">
+                                <label for="region" class="block text-sm font-medium leading-6 text-white">State /
+                                    Province</label>
+                                <div class="mt-2">
+                                    <input type="text" name="region" id="region" autocomplete="address-level1"
+                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                                </div>
+                            </div>
+
+                            <div class="sm:col-span-2">
+                                <label for="postal-code" class="block text-sm font-medium leading-6 text-white">ZIP /
+                                    Postal
+                                    code</label>
+                                <div class="mt-2">
+                                    <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code"
+                                        class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <button type="submit"
+                                class="mt-8 flex w-full justify-center rounded-md bg-danger px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"><a
+                                    href="">Sign up </a></button>
+                                    {/* Already have an account ? <Link to="/login">LogIn</Link> */}
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SignUp
